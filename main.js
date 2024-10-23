e1bca2bbeca0c19b7a27fb2c3fd46e43bd6bd194
sreyareddy1902 <158560047+sreyareddy1902@users.noreply.github.com>
2024-10-23 13:42:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,88 @@
+
+
+//console.log()
+/*--
+console.log("hello world!") //simple msg
+
+let a=25
+console.log(a) //variable
+
+console.log(2+4) //expression
+-----*/
+
+
+//console.error()
+/*---
+console.error("hello world") //simple msg
+
+let age=23
+console.error("Age:" ,age) //variable
+
+console.error("sum:", (2+6))  //expression
+----*/
+
+
+//console.warn()
+/*----
+console.warn("hello world") //msg
+
+let age=23
+console.warn(age) //variable
+
+console.warn(2+6)
+-----*/
+
+//document.write()
+/*---
+document.write("hello world!") //msg or string format
+
+document.write("<h1>This is heading</h1><p>This is paragraph</p>") //tags
+-----*/
+
+
+//document.writeln()
+/*----
+document.writeln("this is first line")
+document.writeln("this is second line")
+
+document.writeln("<h1>This is headingbtag</h1>")
+document.writeln("<p>this is paragraph tag</p>")
+----*/
+
+//window.alert()
+/*---
+alert("hello world")
+
+document.write("hello") //msg
+
+let a=20
+window.alert(a)  //variable
+
+alert(2+7) //expression
+
+function show()
+{
+    alert("hello world")
+}
+show() //function
+-----*/
+
+
+//window.prompt()
+/*---
+let userName=prompt("enter your name?")
+console.log("hello",userName)
+
+let n1=prompt("enter first number:")
+let n2=prompt("enter second number:")
+let sum=Number(n1)+Number(n2)
+alert("sum of two numbers is:",sum)
+console.log(sum)
+-----*/
+
+//window.confirm()
+/*---
+let result=confirm("accept terms and conditions")
+console.log(result)
+----*/
+
